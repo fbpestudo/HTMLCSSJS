250743b9e971b51e8bde004bc52b8198cde4640d
Allan Baliberdin <allan@velorum.com.br>
2018-07-25 23:18:47 -0300

    att this

--- projeto1/javascripts/login2.js
+++ projeto1/javascripts/login2.js
@@ -1,46 +1,48 @@
 
 
 var username = document.getElementById("username");
-var userwrapper = document.getElementById("userwrapper");
+// var userwrapper = document.getElementById("userwrapper");
 var password = document.getElementById("password");
-var passwrapper = document.getElementById("passwrapper");
+// var passwrapper = document.getElementById("passwrapper");
 
 
 
 function checker() {
-    var uservalue = username.value;
-    var passvalue = password.value;
+    var value = this.value;
+    var wrapper = this.parentElement;
+    // var passvalue = password.value;
+    console.log(this.id);
 
-    if (uservalue == undefined || uservalue.length > 0 && uservalue.length < 3 || uservalue.length > 20) {
-        userwrapper.classList.add("error");
-        userwrapper.classList.remove("ok");
+    if (value == undefined || value.length > 0 && value.length < 3 || value.length > 20) {
+        wrapper.classList.add("error");
+        wrapper.classList.remove("ok");
         
     } 
-        else if (uservalue == 0) {
-        userwrapper.classList.remove("error");
-        userwrapper.classList.remove("ok");
+        else if (value == 0) {
+        wrapper.classList.remove("error");
+        wrapper.classList.remove("ok");
     } 
         else {
-        userwrapper.classList.remove("error");
-        userwrapper.classList.add("ok");
+        wrapper.classList.remove("error");
+        wrapper.classList.add("ok");
         
     }
 
-    if (passvalue == undefined || passvalue.length > 0 && passvalue.length < 3 || passvalue.length > 20) {
-        passwrapper.classList.add("error");
-        passwrapper.classList.remove("ok");
+    // if (passvalue == undefined || passvalue.length > 0 && passvalue.length < 3 || passvalue.length > 20) {
+    //     passwrapper.classList.add("error");
+    //     passwrapper.classList.remove("ok");
         
-    } 
-        else if (passvalue == 0) {
-        passwrapper.classList.remove("error");
-        passwrapper.classList.remove("ok");
+    // } 
+    //     else if (passvalue == 0) {
+    //     passwrapper.classList.remove("error");
+    //     passwrapper.classList.remove("ok");
         
-    }
-        else {
-        passwrapper.classList.remove("error");
-        passwrapper.classList.add("ok");
+    // }
+    //     else {
+    //     passwrapper.classList.remove("error");
+    //     passwrapper.classList.add("ok");
         
-    }
+    // }
 
 
 }
